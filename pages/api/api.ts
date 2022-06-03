c9f7123c2417c87052c19bedd796716b1c44be5e
soaza <65228562+soaza@users.noreply.github.com>
2022-06-03 10:45:18 +0800

    Set up API and interfaces

--- pages/api/api.ts
+++ pages/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
+import { IModuleInformation } from "../../utils/interfaces";
 
-const BASE_URL = "http://127.0.0.1:5000";
-
+const BASE_URL = process.env.NEXT_PUBLIC_NUS_MODS_API;
 interface IFetchWrapper {
   url: string;
   method: "GET" | "POST" | "PUT" | "DELETE";
@@ -24,6 +24,17 @@ const axiosWrapper = ({ url, method }: IFetchWrapper) => {
     case "DELETE":
       break;
   }
+};
+
+export const getAllModules: () => Promise<{
+  companies: IModuleInformation[];
+}> = async () => {
+  const res = await axiosWrapper({
+    url: "2021-2022/moduleList.json",
+    method: "GET",
+  });
+
+  return res;
 };
 
 // export const getCompanies: () => Promise<{
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,15 @@
-const IndexPage = () => <h1>Hello Next.js 👋</h1>;
+import { useQuery } from "react-query";
+import { getAllModules } from "./api/api";
 
+const IndexPage = () => {
+  const { isLoading, isError, data, error, refetch } = useQuery(
+    "modules",
+    async () => {
+      const data = await getAllModules();
+      console.log({ data });
+    }
+  );
+
+  return <h1>Hello Next.js 👋</h1>;
+};
 export default IndexPage;
