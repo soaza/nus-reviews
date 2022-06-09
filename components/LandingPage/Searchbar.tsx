fe5a59887f2593e5a449a9d52a0bb1f8452fbacc
Soaza <chukimguan@gmail.com>
2022-06-09 16:20:07 +0800

    Add debounce and loading

--- components/LandingPage/Searchbar.tsx
+++ components/LandingPage/Searchbar.tsx
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
 import { useQuery } from "react-query";
 import { getAllModules } from "../../pages/api/api";
+import useDebounce from "../../utils/hooks";
 
 export const Searchbar = () => {
   const [keyword, setKeyword] = useState("");
   const [modulesFiltered, setModulesFiltered] = useState([]);
+  const debouncedSearch = useDebounce(keyword, 500);
 
-  useQuery(["modules", keyword], async () => {
+  const { isLoading } = useQuery(["modules", debouncedSearch], async () => {
     if (keyword) {
       const data = await getAllModules();
       setModulesFiltered(
         data
           .filter((module) => module.moduleCode.includes(keyword.toUpperCase()))
-          .map((module) => module.moduleCode)
+          .map((module) => `${module.moduleCode}:  ${module.title}`)
           .splice(0, 5)
       );
     }
@@ -21,6 +23,7 @@ export const Searchbar = () => {
   return (
     <div>
       <input
+        autoComplete="off"
         onChange={(e) => setKeyword(e.target.value)}
         type="text"
         name="name"
@@ -32,11 +35,19 @@ export const Searchbar = () => {
         <ul className="absolute bg-white border border-gray-100 w-[90vw] lg:w-[25vw] mt-2">
           {modulesFiltered.map((module) => {
             return (
-              <li className="pl-4 pr-2 py-1 border-b-2 border-gray-100 relative cursor-pointer hover:bg-yellow-50 hover:text-gray-900">
-                <b>{keyword}</b> {module}
+              <li className="pl-4 pr-2 py-1 border-b-2 border-gray-100 relative cursor-pointer hover:bg-blue-50 hover:text-gray-900">
+                {module}
               </li>
             );
           })}
+        </ul>
+      )}
+
+      {isLoading && (
+        <ul className="absolute bg-white border border-gray-100 w-[90vw] lg:w-[25vw] mt-2">
+          <li className="pl-4 pr-2 py-1 border-b-2 border-gray-100 relative cursor-pointer hover:bg-blue-50 hover:text-gray-900">
+            Loading...
+          </li>
         </ul>
       )}
     </div>
