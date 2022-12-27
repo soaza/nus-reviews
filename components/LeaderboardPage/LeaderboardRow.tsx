7f16a9501aea7ef8879b153269ad3ff203173187
soaza <chukimguan@gmail.com>
2022-12-27 09:57:11 +0800

    styling fix

--- components/LeaderboardPage/LeaderboardRow.tsx
+++ components/LeaderboardPage/LeaderboardRow.tsx
@@ -18,14 +18,13 @@ export const LeaderboardRow = (props: {
 
   return (
     <Link href={`/module/${module.module_code}`}>
-      <div className="flex cursor-pointer flex-row justify-around p-4 border-2 rounded-md items-center hover:scale-105 transition ease-in-out delay-150">
-        <div className="text-2xl lg:text-3xl ">{ranking}th</div>
+      <div className="grid grid-cols-3 text-center cursor-pointer justify-around p-4 border-2 rounded-md items-center hover:scale-105 transition ease-in-out delay-150">
+        <div className="text-2xl lg:text-3xl  ">{ranking}th</div>
 
         <div className="flex flex-col">
           <div className="text-2xl lg:text-3xl font-semibold">
             {module.module_code}
           </div>
-          {/* <div className="text-xl font-medium"> Programming Methodology</div> */}
         </div>
 
         <div className="text-xl font-medium ">
